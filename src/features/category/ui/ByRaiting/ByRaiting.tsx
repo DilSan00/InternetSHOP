0eead2e41aae6cd47723805d36281ef00a501a60
DilSan <norms777666@gmail.com>
2024-12-15 00:41:45 +0300

    refactor/R-1-a

--- src/features/category/ui/ByRaiting/ByRaiting.tsx
+++ src/features/category/ui/ByRaiting/ByRaiting.tsx
@@ -3,8 +3,18 @@ const ByRaiting = () => {
     <details>
       <summary>Raiting</summary>
       <div>
-        <h3><input type="checkbox"/>Best</h3>
-        <h3><input type="checkbox"/>bads</h3>
+        <h3>
+          <input type="checkbox" />
+          Best
+        </h3>
+        <h3>
+          <input type="checkbox" />
+          bads
+        </h3>
+        <h3>
+          <input type="checkbox" />
+          Super Best
+        </h3>
       </div>
     </details>
   );
